82654f0e6e45608bd1d2a7bc04bad11a0841cf67
Jean Guanin <84999129+JeanGuaninEspe@users.noreply.github.com>
2024-11-28 02:24:41 -0500

    cambios sockets

--- src/seguridad-espe-vinculacion/sockets/socket.js
+++ src/seguridad-espe-vinculacion/sockets/socket.js
@@ -1,60 +1,150 @@
-import { comprobarJWT } from "../helpers/jwt.js";
-import { io } from "../../app.js";
-import {
+import { enviarNotificacion } from "../helpers/enviar-notificacion.js";
+import models from '../models/index.js';
+
+const { Publicacion, Sala, Mensaje, Usuario, Comentario } = models;
+
+export const usuarioConectado = async (uid = "") => {
+  if (!uid) return null;
+
+  const usuario = await Usuario.findById(uid);
+  if (!usuario) return null;
+
+  usuario.online = true;
+  await usuario.save();
+  return usuario;
+};
+
+export const usuarioDesconectado = async (uid = "") => {
+  if (!uid) return null;
+
+  const usuario = await Usuario.findById(uid);
+  if (!usuario) return null;
+
+  usuario.online = false;
+  await usuario.save();
+  return usuario;
+};
+
+export const grabarMensaje = async (payload) => {
+  try {
+    console.log(payload);
+    const mensaje = new Mensaje(payload);
+    await mensaje.save();
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const grabarMensajeSala = async (payload) => {
+  try {
+    const { mensaje, de, para } = payload;
+    const newMessage = new Mensaje({ mensaje, usuario: de });
+    await newMessage.save();
+
+    const sala = await Sala.findById(para);
+    sala.mensajes.push(newMessage._id);
+    await sala.save();
+
+    const usuariosEnGrupoOffline = await obtenerUsuariosSalaHelper(para, de);
+
+    for (const usuario of usuariosEnGrupoOffline) {
+      if (usuario._id.toString() === de) {
+        continue;
+      }
+
+      usuario.isSalasPendiente = true;
+      usuario.isNotificacionesPendiente = true;
+
+      usuario.salas = usuario.salas.map((sala) => {
+        if (sala.salaId.toString() === para) {
+          sala.mensajesNoLeidos++;
+          sala.ultimaVezActivo = new Date();
+        }
+        return sala;
+      });
+
+      await usuario.save();
+    }
+
+    const tokens = usuariosEnGrupoOffline.map((usuario) => usuario.tokenApp);
+    const titulo = "Nuevo mensaje";
+    const desc = `Tienes un nuevo mensaje en el grupo ${sala.nombre}`;
+
+    const data = {
+      salaId: sala._id,
+      nombre: sala.nombre,
+      mensajesNoLeidos: sala.mensajesNoLeidos,
+      ultimaVezActivo: sala.ultimaVezActivo,
+      type: "sala",
+    };
+
+    const allTokens = [].concat(...tokens);
+    if (allTokens.length === 0) {
+      console.log("No hay tokens");
+      return false;
+    }
+
+    const response = await enviarNotificacion(allTokens, titulo, desc, data);
+    if (response.failure > 0) {
+      console.log("Error al enviar notificación:", response.results);
+    }
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const obtenerUsuariosSalaHelper = async (salaId, usuarioId) => {
+  try {
+    const usuariosEnSala = await Usuario.find({
+      "salas.salaId": salaId,
+      "salas.isRoomOpen": false,
+      _id: { $ne: usuarioId },
+    });
+    return usuariosEnSala;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const grabarComentarioPublicacion = async (payload) => {
+  console.log(payload);
+  const usuarioId = payload.de;
+  try {
+    const { mensaje, para } = payload;
+
+    const publicacion = await Publicacion.findById(para);
+    if (!publicacion) {
+      return res.status(404).json({ error: "Publicación no encontrada" });
+    }
+
+    const comentario = new Comentario({
+      contenido: mensaje,
+      usuario: usuarioId,
+      publicacion: para,
+      estado: "publicado",
+    });
+
+    await comentario.save();
+
+    publicacion.comentarios.push(comentario._id);
+    await publicacion.save();
+
+    return comentario._id.toString();
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export default {
   usuarioConectado,
   usuarioDesconectado,
+  grabarMensaje,
   grabarMensajeSala,
   grabarComentarioPublicacion,
-} from "../controllers/socket.js";
-
-// Mensajes de Sockets
-io.on("connection", (client) => {
-  console.log("Cliente conectado");
-
-  const token = client.handshake.headers["x-token"];
-  if (!token) {
-    console.log("No hay token en la solicitud");
-    return client.disconnect();
-  }
-  console.log(`Token recibido: ${token}`);
-
-  const [valido, uid] = comprobarJWT(token);
-  if (!valido) {
-    console.log("Token no válido");
-    return client.disconnect();
-  }
-
-  usuarioConectado(uid);
-  console.log(`Cliente autenticado: ${uid}`);
-
-  client.on("join-room", async (payload) => {
-    const { codigo } = payload;
-    console.log(`Código de sala recibido: ${codigo}`);
-
-    const [valido, uid] = comprobarJWT(client.handshake.headers["x-token"]);
-    if (!valido) {
-      console.log("Token inválido en join-room");
-      return client.disconnect();
-    } else {
-      client.join(codigo);
-      console.log(`Cliente ${uid} se unió a la sala ${codigo}`);
-    }
-  });
-
-  client.on("mensaje-grupal", async (payload) => {
-    grabarMensajeSala(payload);
-    client.broadcast.to(payload.para).emit("mensaje-grupal", payload);
-    console.log(`Mensaje grupal enviado a ${payload.para}`);
-  });
-
-  client.on("comentario-publicacion", async (payload) => {
-    grabarComentarioPublicacion(payload);
-    client.broadcast.to(payload.para).emit("comentario-publicacion", payload);
-    console.log(`Comentario en publicación enviado a ${payload.para}`);
-  });
-
-  client.on("disconnect", () => {
-    usuarioDesconectado(uid);
-    console.log(`Cliente desconectado: ${uid}`);
-  });
-});
+};
