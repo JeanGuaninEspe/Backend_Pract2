93f01e3133d6fc77b0bcb2d15e934b551757805e
Jean Guanin <84999129+JeanGuaninEspe@users.noreply.github.com>
2025-01-09 05:15:01 -0500

    subida de imagenes prueba2

--- src/seguridad-espe-vinculacion/helpers/subir-archivo.js
+++ src/seguridad-espe-vinculacion/helpers/subir-archivo.js
@@ -1,51 +1,28 @@
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const subirArchivoUsuario = (
   archivo,
   extensionesValidas = ["png", "jpg", "jpeg", "gif"],
   carpeta = ""
 ) => {
-
   return new Promise((resolve, reject) => {
     // console.log(archivo);
     const nombreCortado = archivo.archivo.name.split(".");
     console.log(nombreCortado);
     const extension = nombreCortado[nombreCortado.length - 1];
 
-    const nombreTemp = uuidv4() + "." + extension;
-    const uploadPath = path.join(__dirname, "../../../uploads/", carpeta, nombreTemp);
-
-    archivo.archivo.mv(uploadPath, (err) => {
-      if (err) {
-        reject(err);
-      }
-
-      resolve(nombreTemp);
-    });
-  });
-};
-
-
-export const subirArchivoPublicacion = (
-  archivo,
-  extensionesValidas = ["png", "jpg", "jpeg", "gif"],
-  carpeta = ""
-) => {
-  return new Promise((resolve, reject) => {
-    // console.log(archivo);
-    const nombreCortado = archivo.name.split(".");
-    const extension = nombreCortado[nombreCortado.length - 1];
-
-    // Validar la extensionssssss
     if (!extensionesValidas.includes(extension)) {
-      return reject(
-        `La extensión ${extension} no es permitida - ${extensionesValidas}`
-      );
+      return reject(`La extensión ${extension} no es permitida - ${extensionesValidas}`);
     }
 
     const nombreTemp = uuidv4() + "." + extension;
-    const uploadPath = path.join(__dirname, "../../../uploads/", carpeta, nombreTemp);
+    const uploadPath = path.join(__dirname, "../uploads/", carpeta, nombreTemp);
 
     archivo.mv(uploadPath, (err) => {
       if (err) {
@@ -57,7 +34,29 @@ export const subirArchivoPublicacion = (
   });
 };
 
-export default {
-  subirArchivoUsuario,
-  subirArchivoPublicacion
+export const subirArchivoPublicacion = (
+  archivo,
+  extensionesValidas = ["png", "jpg", "jpeg", "gif"],
+  carpeta = ""
+) => {
+  return new Promise((resolve, reject) => {
+    // console.log(archivo);
+    const nombreCortado = archivo.archivo.name.split(".");
+    const extension = nombreCortado[nombreCortado.length - 1];
+
+    if (!extensionesValidas.includes(extension)) {
+      return reject(`La extensión ${extension} no es permitida - ${extensionesValidas}`);
+    }
+
+    const nombreTemp = uuidv4() + "." + extension;
+    const uploadPath = path.join(__dirname, "../uploads/", carpeta, nombreTemp);
+
+    archivo.mv(uploadPath, (err) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(nombreTemp);
+    });
+  });
 };
