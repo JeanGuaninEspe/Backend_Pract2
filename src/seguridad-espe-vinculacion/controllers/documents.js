39b69bb8f44f5d8dfd4596a057d58488369d4ed7
Jean Guanin <84999129+JeanGuaninEspe@users.noreply.github.com>
2025-02-25 03:43:13 -0500

    implementacion de descarga de archivos 9

--- src/seguridad-espe-vinculacion/controllers/documents.js
+++ src/seguridad-espe-vinculacion/controllers/documents.js
@@ -1,6 +1,5 @@
 import pdf from 'html-pdf';
 import pdfTemplate from '../prueba/public/pdfTemplate.js';
-import path from "path";
 
 export const createDocument = async (req, res) => {
     pdf.create(pdfTemplate(req.body), {}).toBuffer((err, buffer) => {
@@ -14,8 +13,4 @@ export const createDocument = async (req, res) => {
     });
 };
 
-export const getDocument = async (req, res) => {
-    res.sendFile(path.join(__dirname, '../../../uploads/Reporte_IncidentesUE.pdf'));
-};
-
-export default { createDocument, getDocument };
+export default { createDocument };
--- src/seguridad-espe-vinculacion/routes/documents.js
+++ src/seguridad-espe-vinculacion/routes/documents.js
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-// import { check } from "express-validator";
-// import { validarJWT } from "../middlewares/validar-jwt.js";
-// import { validarArchivoSubir } from "../middlewares/validar-archivo.js";
-// import pdfTemplate from '../prueba/public/pdfTemplate.js';
 const router = Router();
 
-import {createDocument,getDocument} from '../controllers/documents.js';
+import { createDocument } from '../controllers/documents.js';
 
-router.post( "/", createDocument );
-
-router.get( "/", getDocument );
+router.post("/", createDocument);
 
 export default router;
