0ed1013f4f5bed5b0460797a91717045b3d87b1b
Jean Guanin <84999129+JeanGuaninEspe@users.noreply.github.com>
2025-02-25 03:34:24 -0500

    implementacion de descarga de archivos 7

--- src/seguridad-espe-vinculacion/controllers/documents.js
+++ src/seguridad-espe-vinculacion/controllers/documents.js
@@ -1,19 +1,16 @@
 import pdf from 'html-pdf';
 import pdfTemplate from '../prueba/public/pdfTemplate.js';
-import path from "path";
 
 export const createDocument = async (req, res) => {
-    pdf.create(pdfTemplate(req.body), {}).toFile('Reporte_IncidentesUE.pdf', (err) => {
+    pdf.create(pdfTemplate(req.body), {}).toBuffer((err, buffer) => {
         if (err) {
-            res.send(Promise.reject());
+            return res.status(500).send('Error al generar el PDF');
         }
 
-        res.send(Promise.resolve());
+        res.setHeader('Content-Disposition', 'attachment; filename=Reporte_IncidentesUE.pdf');
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(buffer);
     });
 };
 
-export const getDocument = async (req, res) => {
-    res.sendFile(path.join(__dirname, '../../../uploads/Reporte_IncidentesUE.pdf'));
-};
-
-export default { createDocument, getDocument };
+export default { createDocument };
